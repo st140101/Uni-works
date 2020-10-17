e2719a089eb7365b4cc7ab8f63923aba6ef44dcf
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-17 21:30:15 +0300

    Update operators_zad3.cpp

--- zadachi ot praktikum/operators_zad3.cpp
+++ zadachi ot praktikum/operators_zad3.cpp
@@ -6,9 +6,12 @@ int main()
 	double lev = 0;
 	double euro = 0;
 	double course = 0.511306792;
+	
 	cout << "Enter in BGN: \n";
 	cin >> lev;
+	
 	euro = lev * course;
 	cout << "That amount in euro is: " << euro;
+	
 	return 0;
 }
