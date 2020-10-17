e98d4f0e1301ad173d8ae8a74639f547068b5364
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-17 21:29:58 +0300

    Update operators_zad4.cpp

--- zadachi ot praktikum/operators_zad4.cpp
+++ zadachi ot praktikum/operators_zad4.cpp
@@ -6,9 +6,12 @@ int main()
 	int a = 0;
 	int b = 0;
 	int power_three = 0;
-	cout << "Enter number one: ";	cin >> a;
-	cout << "Enter number two: ";	cin >> b;
+	
+	cout << "Enter number one: \n";	cin >> a;
+	cout << "Enter number two: \n";	cin >> b;
+	
 	power_three = (a + b) * (a + b) * (a + b);
 	cout << "(a+b)^3 is: " << power_three;
+	
 	return 0;
 }
