77ce574171dcefb9d6628ed360657f641c119101
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-17 21:31:02 +0300

    Update operators_zad5.cpp

--- zadachi ot praktikum/operators_zad5.cpp
+++ zadachi ot praktikum/operators_zad5.cpp
@@ -6,13 +6,21 @@ int main()
 	int number = 0;
 	int reversed_number = 0;
 	int remainder = 0;
-	cout << "Enter a number: ";	cin >> number;
+	
+	
+	cout << "Enter a number: \n";
+	cin >> number;
+	
+	
 	while (number != 0)
 	 {
 		remainder = number % 10;
 		reversed_number = reversed_number * 10 + remainder;
 		number /= 10;
 	 }
+	
+	
 	cout << "That number reversed is: " << reversed_number;
+	
 	return 0;
 }
