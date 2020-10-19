f6e8904de554334de1d902c563ec42f2013f665d
st140101 <siyana.todorova01@gmail.com>
2020-10-19 23:08:09 +0300

    added 1st task

---
+++ zadachi ot praktikum/tasks_bitwise/bitwise_zad1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	
+	cout << "Enter your number: \n";
+	cin >> number;
+
+	if ((number & 1) == 1) {
+		cout << "Your number is uneven." << endl;
+	}
+
+	else {
+		cout << "Your number is even." << endl;
+	}
+	
+	return 0;
+}
