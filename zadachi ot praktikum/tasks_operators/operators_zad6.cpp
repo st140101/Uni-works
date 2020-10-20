282babbf49ec365aeecd1c5d6d8f408dae4475a7
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-20 20:14:41 +0300

    cleaned up code

--- zadachi ot praktikum/tasks_operators/operators_zad6.cpp
+++ zadachi ot praktikum/tasks_operators/operators_zad6.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main()
 {
-	int number = 0, remainder =0, multiplied = 0;
-	double average = 0.0;
-	double sum = 0.0;
-	int digit_one, digit_two, digit_three, digit_four = 0;
-
+	int number = 0;
+	
 	cout << "Enter your number: \n";
 	cin >> number;
 	
+	int digit_one, digit_two, digit_three, digit_four = 0;
 	digit_one = number % 10;
 	digit_two = number / 10 % 10;
 	digit_three = number / 100 % 10;
 	digit_four = number / 1000 % 10;
-
+        
+	int multiplied = 0;		
+	double average, sum = 0.0;
 	sum = digit_one + digit_two + digit_three + digit_four;
 	multiplied = digit_one * digit_two * digit_three * digit_four;
 	average = sum / 4;
