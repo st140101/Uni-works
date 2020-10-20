4cefd04fe9a1e9b72c5574f8983b2f3f9923bd1d
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-20 20:24:21 +0300

    removed empty lines

--- zadachi ot praktikum/tasks_operators/operators_zad5.cpp
+++ zadachi ot praktikum/tasks_operators/operators_zad5.cpp
@@ -6,20 +6,17 @@ int main()
 	int number = 0;
 	int reversed_number = 0;
 	int remainder = 0;
-	
-	
+		
 	cout << "Enter a number: \n";
 	cin >> number;
-	
-	
+		
 	while (number != 0)
 	 {
 		remainder = number % 10;
 		reversed_number = reversed_number * 10 + remainder;
 		number /= 10;
 	 }
-	
-	
+		
 	cout << "That number reversed is: " << reversed_number;
 	
 	return 0;
