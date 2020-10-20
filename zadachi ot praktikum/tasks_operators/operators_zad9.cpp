7ec1723dc398ab1fb6b2064c7a8f5482acf6624c
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-20 20:19:07 +0300

    removed empty lines moved variable declarations

--- zadachi ot praktikum/tasks_operators/operators_zad9.cpp
+++ zadachi ot praktikum/tasks_operators/operators_zad9.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main()
 {
 	int a, b, c = 0;
-	int x1, x2 = 0; // x1 and x2 refer to the roots of the equation
-	int discriminant = 0;
-	
-
+		
 	cout << "Enter coefficient 'a' for ax^2 + bx + c = 0 : \n";
 	cin >> a;
 	
@@ -19,12 +16,12 @@ int main()
 	cin >> c;
 
 	cout << "The quadratic equation is: " << a << "x^2+" << b << "x+" << c << "." << endl;
-
-
+	
+	int discriminant = 0;
 	discriminant = b * b - 4 * a * c;
-
+        
+	int x1, x2 = 0; // x1 and x2 refer to the roots of the equation
 	if (discriminant == 0) {
-		x1 == x2;
 		x1 = (-b) / (2 * a);
 		
 		cout << "The quadratic equation" << a << "x^2+" << b << "x+" << c << " has one real root " << x1 << endl;
