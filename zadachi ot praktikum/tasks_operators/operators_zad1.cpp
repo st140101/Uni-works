2aa94a86c53414d5cb0f786496936445b4993fa6
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-10-20 20:28:22 +0300

    moved variable declaration

--- zadachi ot praktikum/tasks_operators/operators_zad1.cpp
+++ zadachi ot praktikum/tasks_operators/operators_zad1.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main()
 {
-	int number_one, number_two, difference = 0;
+	int number_one, number_two;
 	
 	cout << "Enter your first number: \n";
 	cin >> number_one;
 	cout << "Enter your second number: \n";
 	cin >> number_two;
 
+	int difference = 0;
 	difference = number_one - number_two;
 
 	cout << "The difference " << number_one << "-" << number_two << " is = " << difference;
