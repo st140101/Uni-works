b2cfba112c7a3f632c87accc36875b23675c3f5d
Siyana Todorova <72251110+st140101@users.noreply.github.com>
2020-11-26 15:50:25 +0200

    final ver zad3

--- zad3.cpp
+++ zad3.cpp
@@ -18,9 +18,11 @@
 using namespace std;
 vector<vector<int>> combined;
 
-int UserInput() {
+int userInput() {
+    
     int inp;
     const int from = 3, to = 1000;
+    
     do {
         cin >> inp;
     } while (inp < from || inp > to);
@@ -29,34 +31,30 @@ int UserInput() {
 }
 
 int digitsInput() {
+    
     int inp;
     const int min = 0;
+    
     do {
         cin >> inp;
     } while (inp < min);
 
     return inp;
 }
-bool searchVec(vector<int> value) { // check if element exists
+bool existsInVec(vector<int> value) { // check if element exists
+   
     for (int i = 0; i < combined.size(); i++) {
         if (combined[i] == value) {
             return true;
-        }
-        else {
-            for (int i = 0; i < combined.size(); ++i) {
-                for (int j = 0; j < combined[i].size(); ++j) {
-                    cout << combined[i][j] << " ";
-                    cout << endl;
-                }
-            }
         }
     }
     return false;
 }
 
-
-bool is_pally(const vector<int> v) {
+bool isPalindrome(const vector<int> v) {
+    
     vector<int> reverse;
+
     for (int i = v.size() - 1; i >= 0; i--) {
         reverse.push_back(v[i]);
     }
@@ -69,48 +67,34 @@ void checkPalin(vector<int> v) {
         return;
     }
 
-    if (is_pally(v)) {
-
-        for (int i = 0; i < v.size(); i++) {
-            cout << v[i] << " ";
-        }
-        cout << endl;
-
-        if (searchVec(v) == false) {
+    if (isPalindrome(v)) {
+        if (existsInVec(v) == false) {
             combined.push_back(v);
         }
-
     }
+
     vector<int> left_side = v, right_side = v;
 
     left_side.erase(left_side.begin());
     right_side.pop_back();
 
-
     checkPalin(left_side);
     checkPalin(right_side);
-    is_pally(v);
 }
 
 int main() {
-    int numOfdigits = UserInput();
+    
+    int numOfdigits = userInput();
 
     vector<int> enteredDigits;
 
     for (int i = 0; i < numOfdigits; i++) {
-
         int temp = digitsInput();
         enteredDigits.push_back(temp);
     }
-    checkPalin(enteredDigits);
-    for (int i = 0; i < combined.size(); ++i) {
-        for (int j = 0; j < combined[i].size(); ++j) {
-            cout << combined[i][j] << " ";
-            cout << endl;
-        }
-    }
-    cout << "\npalindrome count: ";
 
-    cout << "* " << combined.size() << "*" << endl;
+    checkPalin(enteredDigits);
+
+    cout << combined.size() << endl;
     return 0;
 }
